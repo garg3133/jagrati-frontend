c2f52f141cdfc016ebf8f5c48fcfb596ab8d18ca
Priyansh Garg <priyanshgarg30@gmail.com>
2021-02-05 00:04:59 +0530

    Modify naming of index route.

--- src/routes/index/Home.js
+++ src/routes/index/Home.js
@@ -1,5 +1,3 @@
-import Header from 'components/home/Header'
-import Navbar from 'components/home/Navbar'
 import Showcase from 'components/home/Showcase'
 import Contact from 'components/home/Contact'
 
@@ -7,8 +5,6 @@ import Contact from 'components/home/Contact'
 const Home = () => {
     return (
         <div className="container">
-            <Header />
-            <Navbar />
             <Showcase />
             <Contact />
         </div>
